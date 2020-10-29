aa392afdcb5a89604d95c0d2f7b5cca381127098
Casey Knolla <cknolla@gmail.com>
2020-10-28 22:54:04 -0700

    fixed line endings globally and added password hashing

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 VAULT_TOKEN=$(curl \
+--silent \
 --request POST \
 --cert "$HOME/ansible.kansai.pem" \
 --key "$HOME/ansible.kansai.key" \
 "$VAULT_ADDR/v1/auth/cert/login" | jq -r '.auth.client_token')
 
 SECRET_DATA=$(curl \
+--silent \
 --request GET \
 --header "X-Vault-Token: $VAULT_TOKEN" \
 "$VAULT_ADDR/v1/kv/data/ansible" | jq -r '.data.data.secret')
 
-echo "$SECRET_DATA"
+ansible-playbook -K playbooks/common.yml --extra-vars "user_password={{ $SECRET_DATA | password_hash('sha512', 'somesalt') }}"
