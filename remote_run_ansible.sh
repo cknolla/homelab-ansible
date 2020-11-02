7aba720f92b52d207a403e18cebf21bed90d9bc2
Casey Knolla <cknolla@gmail.com>
2020-11-02 05:46:52 -0800

    make remote run more generic

--- remote_run_ansible.sh
+++ remote_run_ansible.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+# useful for setting up in an IDE's run configuration
+
 if [[ -z $1 ]]; then
   echo "Must provide ansible username" >&2
+  exit
 fi
 
 if [[ -z $2 ]]; then
   echo "Must provide ansible hostname" >&2
+  exit
 fi
 
-ssh -t "$1"@"$2" 'cd homelab-ansible && ./run_ansible.sh'
+if [[ -z $3 ]]; then
+  echo "Must provide remote working directory" >&2
+  exit
+fi
+
+ssh -t "$1"@"$2" "cd $3 && ./run_ansible.sh"
